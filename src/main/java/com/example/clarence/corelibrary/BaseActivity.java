635e12b50f2334a34874e6de8b0fc7c31fafcafc
caishuxing <caishuxing@linggan.com>
2016-09-23 11:02:54 +0800

    添加共用头部

--- src/main/java/com/example/clarence/corelibrary/BaseActivity.java
+++ src/main/java/com/example/clarence/corelibrary/BaseActivity.java
@@ -1,13 +1,74 @@
 package com.example.clarence.corelibrary;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /**
  * 预留扩展用
  * Created by clarence on 16/5/29.
  */
 public class BaseActivity extends FragmentActivity {
+    private View mRootView;
+    private LinearLayout baseLayout;
+    public TitleBarCommon titleBarCommon;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initBaseView();
+        initTitleBar();
+    }
 
+    @Override
+    public void setContentView(View view) {
+        addChildView(view);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        View view = getLayoutInflater().inflate(layoutResID, null);
+        addChildView(view);
+    }
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        addChildView(view);
+    }
+
+    private void initBaseView() {
+        mRootView = getLayoutInflater().inflate(R.layout.base_layout, null);
+        super.setContentView(mRootView);
+        baseLayout = (LinearLayout) findViewById(R.id.base_layout);
+        titleBarCommon = (TitleBarCommon) findViewById(R.id.head_layout);
+    }
+
+    protected void initTitleBar() {
+        setTitlebarAction();
+    }
+
+    protected void setTitlebarAction() {
+        if (titleBarCommon != null) {
+            if (titleBarCommon.getLeftButtonView() != null) {
+                titleBarCommon.getLeftButtonView().setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                BaseActivity.this.finish();
+                            }
+                        });
+            }
+        }
+    }
+
+    private void addChildView(View view) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT,
+                RelativeLayout.LayoutParams.FILL_PARENT);
+        if(baseLayout!=null){
+            baseLayout.addView(view, params);
+        }
+    }
 }
 
---
+++ src/main/java/com/example/clarence/corelibrary/TitleBarCommon.java
@@ -0,0 +1,104 @@
+package com.example.clarence.corelibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by caishuxing on 2015/7/1.
+ */
+public class TitleBarCommon extends RelativeLayout {
+    private TextView mTvTitle;
+    private ImageView mIvLeft;
+    private View mTitleContainer;
+    private Context context;
+
+    public TitleBarCommon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        this.mTitleContainer = LayoutInflater.from(context).inflate(R.layout.base_header, this, true);
+        init();
+    }
+
+    public void init() {
+        mTvTitle = (TextView) mTitleContainer.findViewById(R.id.baselayout_tv_title);
+        mIvLeft = (ImageView) mTitleContainer.findViewById(R.id.baselayout_iv_left);
+    }
+
+
+    /**
+     * 自定义头部
+     **/
+    public void setCustomTitleBar(int customTitleLayoutId) {
+        if (customTitleLayoutId <= 0) {
+            this.setVisibility(View.GONE);
+            return;
+        }
+        removeAllViews();
+        View baseHead = View.inflate(context, customTitleLayoutId, null);
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        addView(baseHead, params);
+    }
+
+    /**
+     * 设置标题文字 (若要隐藏设置参数为-1)
+     *
+     * @param resId
+     */
+    public TitleBarCommon setTitle(int resId) {
+        if (resId != -1) {
+            mTvTitle.setText(resId);
+            mTvTitle.setVisibility(View.VISIBLE);
+        } else {
+            mTvTitle.setVisibility(View.GONE);
+        }
+        return this;
+    }
+
+    /**
+     * 设置标题名称
+     *
+     * @param str
+     * @return
+     */
+    public TitleBarCommon setTitle(String str) {
+        try {
+            mTvTitle.setText(str);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return this;
+    }
+
+    /**
+     * 获取左边按钮控件
+     *
+     * @return
+     */
+    public View getLeftButtonView() {
+        return mIvLeft;
+    }
+
+
+    /**
+     * 获取titleBar布局
+     *
+     * @return
+     */
+    public View getTitleBar() {
+        return mTitleContainer;
+    }
+
+
+    /****
+     * 隐藏返回键
+     */
+    public void hideBack() {
+        mIvLeft.setVisibility(View.GONE);
+    }
+}
